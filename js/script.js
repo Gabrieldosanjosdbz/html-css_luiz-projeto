9bcc970ea10d1921c35d7056ceb6db4214c9b8c0
gabriel <gabrieldosanjosdbz@gmil.com>
2024-05-16 09:24:24 -0300

    create script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Este código será executado apenas após o conteúdo HTML ser totalmente carregado
+    // Aqui você pode fazer o que desejar, como mostrar a tela
+    document.body.style.display = 'block'; // Exibe a tela
+  
+    // Se houver outras operações que você queira fazer após o carregamento completo, você pode colocá-las aqui
+  });
