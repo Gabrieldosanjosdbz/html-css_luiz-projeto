15fc3d7a61fedc1f7a7edf03e140bd0887b1bc36
unknown <geananet2@gmail.com>
2024-05-20 13:41:35 -0300

    placeholder img

--- projetos/script_ensaio.js
+++ projetos/script_ensaio.js
@@ -1,20 +1,24 @@
 const lazyLoading = () => {
-    const listaElementos = document.querySelectorAll("[data]")
+    const listaElementos = document.querySelectorAll("img[data]");
 
     listaElementos.forEach((elemento) => {
-        if(elemento.getBoundingClientRect().top < window.innerHeight) {
-            elemento.src = elemento.getAttribute("data")
-            elemento.removeAttribute("data")
-            
-            console.log("carregou")
+        if (elemento.getBoundingClientRect().top < window.innerHeight) {
+            const src = elemento.getAttribute("data");
+            elemento.setAttribute("src", src);
+            elemento.removeAttribute("data");
+
+            elemento.onload = () => {
+                elemento.style.opacity = 1; // Mostra a imagem carregada
+                const placeholder = elemento.previousElementSibling;
+                if (placeholder && placeholder.classList.contains("placeholder")) {
+                    placeholder.remove(); // Remove o placeholder
+                }
+            };
+
+            console.log("carregou");
         }
-    })
-}
+    });
+};
 
-window.onload = () => {
-    lazyLoading()
-}
-
-window.onscroll = () => {
-    lazyLoading()
-}
+window.addEventListener('load', lazyLoading);
+window.addEventListener('scroll', lazyLoading);
